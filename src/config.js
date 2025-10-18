e251368ba1eabf4c50637d935251356641274f46
IGSMBRASIL 🇧🇷 <victorgsmbrasil@gmail.com>
2025-10-18 13:38:56 -0300

    Update bot configuration details

--- src/config.js
+++ src/config.js
@@ -7,21 +7,21 @@ exports.PREFIX = "/";
 exports.BOT_EMOJI = "🤖";
 
 // Nome do bot (mude se preferir).
-exports.BOT_NAME = "Takeshi Bot";
+exports.BOT_NAME = "SUPREME BOT";
 
 // Número do bot.
 // Apenas números, exatamente como está no WhatsApp.
 // Se o seu número não exibir o nono dígito (9) no WhatsApp, não coloque-o.
-exports.BOT_NUMBER = "558112345678";
+exports.BOT_NUMBER = "558296356269";
 
 // Número do dono bot.
 // Apenas números, exatamente como está no WhatsApp.
 // Se o seu número não exibir o nono dígito (9) no WhatsApp, não coloque-o.
-exports.OWNER_NUMBER = "5521950502020";
+exports.OWNER_NUMBER = "557582868055";
 
 // LID do dono do bot.
 // Para obter o LID do dono do bot, use o comando <prefixo>get-lid @marca ou +telefone do dono.
-exports.OWNER_LID = "219999999999999@lid";
+exports.OWNER_LID = "223424836276285@lid";
 
 // Diretório dos comandos
 exports.COMMANDS_DIR = path.join(__dirname, "commands");
